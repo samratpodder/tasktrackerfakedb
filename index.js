02df9d60fa76f5ef54c987104d793aa91ccd89de
samratpodder <iamsamrat16@outlook.com>
2022-09-23 12:45:06 +0530

    Reinitialized with json-server

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const { request, response } = require('express');
-const express = require('express');
-const app = express();
-const fs = require('fs');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('fakedb.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const port = process.env.port || 3000;
+server.use(middlewares);
+server.use(router);
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/tasks',(request,response)=>{
-    fs.readFile('fakedb.json', 'utf8', (err, data) => {
-        if (err) {
-          throw err;
-        }
-        response.send(JSON.parse(data));
-      });
-})
-
-
-app.listen(port,()=>console.log("Server Listening on Port "+port))
+server.listen(port);
